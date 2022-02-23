859bd780ab5db82fbbedbddd96e2944900c00e02
Giovana Furlan <giovananelofurlan@gmail.com>
2022-02-23 17:05:58 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import { Box, Button, ChakraProvider, Divider, Heading, Input, Text } from "@chakra-ui/react";
+import { Avatar, Box, Button, ChakraProvider, Divider, Heading, Input, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import dateFormat from "dateformat";
+
 
 function App() {
 
-  const data = [
-    {
-      name: 'Brinquedos',
-      resultados: 915,
-    },
-    {
-      name: 'Alimentos',
-      resultados: 489,
-    },
-    {
-      name: 'Casa',
-      resultados: 44
-    },
-    {
-      name: 'Beleza',
-      resultados: 37
-    },
-    {
-      name: 'Livros',
-      resultados: 37
-    },
-    {
-      name: 'Festas',
-      resultados: 5
-    },
-    {
-      name: 'Esportes',
-      resultados: 1
-    }
-  ];
+  const data = [];
   
   const data2 = [
     {
@@ -81,19 +54,57 @@ function App() {
 
 const axios = require('axios');
 
-const token= "APP_USR-8685824602582630-022313-3ae270f8be2bdddb9bdcbca13fed0f7f-398092563";
-function send() {
+const token= "APP_USR-8685824602582630-022319-237cc772e3cbaee636f2827cf5ee9f1a-398092563";
+function Send() {
 
-  axios.get('https://api.mercadolibre.com/users/'+ document.getElementById("searchTerm").value, {
+  axios.get('https://api.mercadolibre.com/sites/MLB/search?nickname='+ document.getElementById("searchTerm").value, {
     headers: {'Authorization': `Bearer ${token}`},
   })
   .then(function (response) {
     console.log(response.data);
-    const nickname= response.data.nickname;
+
+    var logo= response.data.seller.eshop.eshop_logo_url;
+    // document.getElementById("logo_id").textContent= logo;
+    
+    const newEle = (<Avatar size='2xl' mt="1vw" mb="1vw" name='User Logo' src= {logo}>Logo</Avatar>)
+    document.getElementById("logo_id").append(logo);
+    console.log(newEle);
+
+    const nickname= response.data.seller.nickname;
     document.getElementById("nickname_id").textContent= nickname;
 
-    const id= response.data.id;
+    const id= response.data.seller.id;
     document.getElementById("id_id").textContent= id;
+
+    const permalink= response.data.seller.permalink;
+    document.getElementById("permalink_id").textContent= permalink;
+
+    const date= response.data.seller.registration_date;
+    document.getElementById("rdate_id").textContent= dateFormat(date);
+
+    const power= response.data.seller.seller_reputation.power_seller_status;
+
+    const level= response.data.seller.seller_reputation.level_id;
+    if (level === "5_green" && power === "platinum"){
+      document.getElementById("level_id1").textContent= "MercadoLíder Platinum";
+    }
+    if (level === "5_green" && power === "gold"){
+      document.getElementById("level_id1").textContent= "MercadoLíder Gold";
+    }
+  
+    const completed= response.data.seller.seller_reputation.transactions.completed;
+    document.getElementById("tcompleted_id").textContent= completed;
+
+    const canceled= response.data.seller.seller_reputation.transactions.canceled;
+    document.getElementById("tcanceled_id").textContent= canceled;
+
+    for (var i = 0; i < response.data.available_filters.values.length; i++) {
+      const linhaData = response.data.available_filters.values.results[i];
+      data.push({
+        name: linhaData.name,
+        resultados: linhaData.results
+      })
+    }
   })
   .catch(function (error) {
     console.error(error);
@@ -110,41 +121,43 @@ function send() {
           <Box p="1vw">
             <Text fontSize='md' noOfLines={1} mb="1vw" fontWeight={500}>Seller Nickname</Text>
             <Input id="searchTerm" noOfLines={1} w="20vw" placeholder='Seller Nickname' bg="white" />
-            <Button bg='#319795' color="white" mt="1vw" onClick={send}>Submit</Button>  
+            <Button bg='#319795' color="white" mt="1vw" onClick={Send}>Submit</Button>  
           </Box>
           <Box m="1vw">
-            <Text fontSize='md' mt="1vw">User</Text>
+            <Text fontSize='md' mt="1vw" fontWeight={500}>User</Text>
+            <Box id="logo_id">                
+              {/* <Avatar size='2xl' mt="1vw" mb="1vw" name='User Logo' src='' /> */}
+            </Box>
+            <Box>
             <Box>                
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Nickname:</Text>
-              <Text fontSize='md' noOfLines={1} id="nickname_id"></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">Nickname:</Text>
+              <Text fontSize='3xl' noOfLines={1} id="nickname_id"></Text>
             </Box>
             <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">User ID:</Text>
-              <Text fontSize='md' noOfLines={1} id="id"></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">User ID:</Text>
+              <Text fontSize='md'noOfLines={1} id="id_id"></Text>
             </Box>
             <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Permalink: </Text>
-              <Text fontSize='md' noOfLines={1}></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">Permalink: </Text>
+              <Link fontSize='md' noOfLines={1} id="permalink_id" href='' isExternal></Link>
             </Box>
             <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Registration Date: </Text>
-              <Text fontSize='md' noOfLines={1}></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">Registration Date: </Text>
+              <Text fontSize='md' noOfLines={1} id="rdate_id"></Text>
             </Box>
             <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Level: </Text>
-              <Text fontSize='md' noOfLines={1}></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">Level: </Text>
+              <Text fontSize='md' noOfLines={1}fontWeight={500} id="level_id1" color='green'></Text>
+              <Text fontSize='md' noOfLines={1}fontWeight={500} id="level_id2" color='red'></Text>
             </Box>
             <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Power Seller Status: </Text>
-              <Text fontSize='md' noOfLines={1}></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">Transactions Completed: </Text>
+              <Text fontSize='md' noOfLines={1} id="tcompleted_id"></Text>
             </Box>
             <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Transactions Completed: </Text>
-              <Text fontSize='md' noOfLines={1}></Text>
+              <Text as='u' fontSize='md'fontWeight={500} noOfLines={1} mt="1vw">Transactions Canceled: </Text>
+              <Text fontSize='md' noOfLines={1} id="tcanceled_id"></Text>
             </Box>
-            <Box>
-              <Text as='u' fontSize='md' noOfLines={1} mt="1vw">Transactions Canceled: </Text>
-              <Text fontSize='md' noOfLines={1}></Text>
             </Box>
             </Box>
             <Divider />
